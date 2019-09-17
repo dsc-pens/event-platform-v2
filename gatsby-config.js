59f7c6fdd5a850472f17b98048443067bef24178
maxxoto <ahmatdanis24@gmail.com>
2019-09-17 12:14:31 +0700

    :cyclone: add gatsby-plugin-purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
         icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-purgecss",
+      options: {
+       
+      }
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
